aba6e3ba2f8d2d919a1a851fe5768513cb3afa1d
horan <13571899655@163.com>
2026-01-20 13:04:07 +0800

    fix: error

--- bootstrap.lua
+++ bootstrap.lua
@@ -10,7 +10,7 @@ local http_response = require("lib.response")
 local routes = require("routes")
 
 -- Run our application
-local response = application:run(routes)
+local response = application.run(routes)
 
 -- send response to user
 http_response:send(response)
